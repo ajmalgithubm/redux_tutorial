1c90bbd42a92e84ecb60dc0679e8d58f9f2adee1
ajmalgithubm <ajmalm76774@gmail.com>
2023-08-27 20:06:31 +0530

    task reducer created

--- src/reducers/taskReducers.js
+++ src/reducers/taskReducers.js
@@ -1,3 +1,14 @@
 const initialState = {
     tasks : []
 }
+
+const taskReducers = ( state= initialState, action) => {
+    switch (action.type) {
+        case "ADD_TASK":
+            return { ...state, tasks:[ ... state.tasks, action.payLoad] }
+        case "DELETE_TASK":
+            return { ...state, tasks: state.tasks.filter(task => task.id !== action.payLoad)}
+        default:
+            return state
+    }
+}
